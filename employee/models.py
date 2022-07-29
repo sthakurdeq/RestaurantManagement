1d652cf4ee7a8e4043653880d3a0b65ce6796446
Priyanshu Gupta <prgupta@deqode.com>
2022-07-30 00:45:17 +0530

    Doc strings and comments added in classes

--- employee/models.py
+++ employee/models.py
@@ -6,6 +6,12 @@ from django.utils.translation import gettext as _
 
 
 class AbstructBaseModel(models.Model):
+    '''
+    Base model to inherit in rest models
+    id: UUID field(unique)
+    created_at: Date time field default is current timestamp
+    updated_at: Date time field default is current timestamp
+    '''
     class Meta:
         abstract = True
 
@@ -14,6 +20,11 @@ class AbstructBaseModel(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Updated At"))
 
 class User(AbstractUser):
+    '''
+    User model to add fields in default user model
+    id: UUID field(unique)
+    email: User/Employee email
+    '''
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(_("email address"), unique=True)
 
--- employee/views.py
+++ employee/views.py
@@ -13,18 +13,22 @@ from django.views.decorators.csrf import csrf_exempt
 class UserViewSet(viewsets.ModelViewSet):
     """
     Create a Users Accepts POST,GET
+    return fields as per the UserSerializer
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
     def update(self, request, pk=None):
+        # returns forbidden when PUT request
         response = {'message': 'Update function is not offered in this path.'}
         return Response(response, status=status.HTTP_403_FORBIDDEN)
 
     def partial_update(self, request, pk=None):
+        # returns forbidden when PATCH request
         response = {'message': 'Update function is not offered in this path.'}
         return Response(response, status=status.HTTP_403_FORBIDDEN)
 
     def destroy(self, request, pk=None):
+        # returns forbidden when DELETE request
         response = {'message': 'Delete function is not offered in this path.'}
         return Response(response, status=status.HTTP_403_FORBIDDEN)
