673ad4e07281b58bbb94a375e5cacd76a07ee388
Priyanshu Gupta <prgupta@deqode.com>
2022-07-29 12:50:44 +0530

    Field changes

--- restaurant/models.py
+++ restaurant/models.py
@@ -43,10 +43,27 @@ class Menu(AbstructBaseModel):
     day : Days ('MON','TUE','WED','THUR','FRI')
     item : many to many field to link item to restaurant menu
     '''
-    DAY=(('MON','Monday'),('TUE','Tuesday'),('WED','Wednesday'),('THUR','Thursday'),('FRI','Friday'))
-    VOTE=((-1,-1),(0,0),(1,1))
+    class DAY   :
+        MON = "MON"
+        TUE = "TUE"
+        WED = "WED"
+        THUR = "THUR"
+        FRI = "FRI"
+    DAY_CHOICES = [
+        (DAY.MON, "Monday"),
+        (DAY.TUE, "South Indian"),
+        (DAY.WED, "Wednesday"),
+        (DAY.THUR, "Thursday"),
+        (DAY.FRI,'Friday')
+    ]
+
+    VOTE_CHOICES=[
+        (-1,"Dislike"),
+        (0,"Neutral"),
+        (1,"Like")
+    ]
     restaurant_name=models.ForeignKey(Restaurant,on_delete=models.CASCADE)
-    day=models.CharField(max_length=10,choices=DAY)
-    vote=models.IntegerField(choices=VOTE)
+    day=models.CharField(max_length=10,choices=DAY_CHOICES)
+    vote=models.IntegerField(choices=VOTE_CHOICES)
     item=models.ManyToManyField(Item)
 
