4517604c36042079e96976afe98ddfea7203b165
Priyanshu Gupta <prgupta@deqode.com>
2022-07-29 15:14:18 +0530

    updated settings

--- restaurantManagement/settings.py
+++ restaurantManagement/settings.py
@@ -83,22 +83,22 @@ WSGI_APPLICATION = 'restaurantManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME':'restaurant',
-#         'USER':'tester',
-#         'PASSWORD':'test_password',
-#         'HOST':'localhost',
-#         'PORT':'5432'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'restaurant',
+        'USER':'tester',
+        'PASSWORD':'test_password',
+        'HOST':'localhost',
+        'PORT':'5432'
+    }
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
