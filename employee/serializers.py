3b25ee5b5fea20a211365f0054025b6e84f72525
Priyanshu Gupta <prgupta@deqode.com>
2022-07-29 13:38:39 +0530

    Serializers change

--- employee/serializers.py
+++ employee/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from .models import User
-from datetime import date
+from datetime import date, datetime
 import uuid
 
 class UserSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(default=uuid.uuid4, editable=False)
-    email = serializers.EmailField(unique=True)
-    created_at = serializers.DateTimeField(auto_now_add=True)
-    updated_at = serializers.DateTimeField(auto_now=True)
+    id = serializers.UUIDField(default=uuid.uuid4)
+    email = serializers.EmailField()
+    created_at = serializers.DateTimeField(default=datetime.today)
+    updated_at = serializers.DateTimeField(default=datetime.today)
 
     class Meta:
         model = User
